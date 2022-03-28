ea154d00999f6a5d55448dc82a62b0a6833a48ac
hadoop-vacv <hadoop-vacv@set-zw-yarn-training454782.mt>
2022-03-28 17:19:34 +0800

    init

---
+++ mmdet/datasets/samplers/__init__.py
@@ -0,0 +1,5 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+from .distributed_sampler import DistributedSampler
+from .group_sampler import DistributedGroupSampler, GroupSampler
+
+__all__ = ['DistributedSampler', 'DistributedGroupSampler', 'GroupSampler']
